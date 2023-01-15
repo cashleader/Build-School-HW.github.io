59eaecd1b43394f4a2c1804e28614a7a0fc474ba
Jin <cashleader0913@gmail.com>
2023-01-16 00:26:39 +0800

    mapV2

--- JSHomeWork6/map.js
+++ JSHomeWork6/map.js
@@ -1,11 +1,12 @@
 //宣告
-let map
-const areaDataRequest = fetch('./台灣行政地區.json')
-const carDataRequest = fetch('./111年臺北市電動機車充電地點.json')
-let areaData //行政區資料
-let carData //充電站量資料
-let carAreaData //充電站資料 + 行政區經緯度資料
-let markers = L.markerClusterGroup()  //marker叢集
+let map;
+const areaDataRequest = fetch('./台灣行政地區.json');
+const carDataRequest = fetch('./111年臺北市電動機車充電地點.json');
+let areaData; //行政區資料
+let carData; //充電站量資料
+let carAreaData; //充電站資料 + 行政區經緯度資料
+let markers = L.markerClusterGroup();  //marker叢集
+
 //DOM
 const countySelect = document.querySelector('#county')
 const countyText = document.querySelector('strong')
@@ -14,7 +15,7 @@ const tbody = document.querySelector('tbody')
 function initMap() {
     // 初始地圖
     map = L.map('map', {
-        center: [25.03416068163684, 121.56454962636319],
+        center: [25.084559885704355, 121.52489910527188],
         zoom: 9
     })
     // 設定圖層 openstreetmap
@@ -22,6 +23,7 @@ function initMap() {
     let osm = new L.TileLayer(osmUrl, { minZoom: 8, maxZoom: 19 })
     map.addLayer(osm)
 }
+
 function setMapItem() {
     Promise.all([areaDataRequest, carDataRequest])
         .then(res => Promise.all(res.map(x => x.json())))
@@ -35,31 +37,66 @@ function setMapItem() {
                     lat: area != undefined ? area.Lat : undefined,
                     lng: area != undefined ? area.Lng : undefined
                 }
-            }).filter(x => x.lang != undefined).groupBy('Town');
+            }).filter(x => x.lng != undefined).groupBy('Town');
+            console.log(carAreaData)
             // renderMarker();
             // initCountySelect();
-            console.log(carAreaData);
+
         })
 }
-// function initCountySelect() {
-//     ['請選擇', ...new Set(areaData.map(x => x.Town))].forEach(city => {
-//         let option = document.createElement('option')
-//         option.innerText = Town
-//         option.value = Town == '請選擇' ? '' : Town
-//         countySelect.appendChild(option)
-//     })
+function renderMarker() {
+    if (markers) markers.clearLayers();
+    Object.keys(carAreaData).forEach(key => {
+        let data = carAreaData[key]
+        let marker = L.marker([data[0].lat], data[0].lng)
+        marker.bindPopup(
+            `
+            <h4>${data[0].Town}</h4>
+            `
+        )
+        marker.addEventListener('click', function() {
+            countyText.innerText = data[0].Town;
+            tbody.innerHTML = '';
 
-//     countySelect.onchange = function () {
-//         if (this.value != '') {
-//             // 改變地圖的焦點
-//             let county = areaData.find(x => x.Town == this.value)
-//             map.setView([county.Lat, county.Lng], 12)
-//         }
-//     }
-// }
+            let nameTr = document.createElement('tr');
+            let addressTr = document.createElement('tr');
+            let otherTr = document.createElement('tr');
+            data.forEach(x => {
+                let nameTd = document.createElement('td');
+                let addressTd = document.createElement('td');
+                let otherTd = document.createElement('td');
+                nameTd.innerText = `${x.name}`
+                addressTd.innerText=`${x.adress}`
+                otherTd.innerText=`${x.other}`
 
+                nameTr.appendChild(nameTd)
+                addressTr.appendChild(addressTd);
+                otherTr.appendChild(otherTd)
+            })
+            tbody.appendChild(nameTr)
+            tbody.appendChild(addressTr)
+            tbody.appendChild(otherTr)
+        })
+        markers.addLayer(marker)
+    })
+    map.addLayer(markers)
+}
+function initCountySelect() {
+    ['請選擇', ...new Set(areaData.map(x => x.Town))].forEach(city => {
+        let option = document.createElement('option')
+        option.innerText = Town
+        option.value = Town == '請選擇' ? '' : Town
+        countySelect.appendChild(option)
+    })
 
-
+    countySelect.onchange = function () {
+        if (this.value != '') {
+            // 改變地圖的焦點
+            let county = areaData.find(x => x.Town == this.value)
+            map.setView([county.Lat, county.Lng])
+        }
+    }
+}
 
 //window.onload 
 window.onload = function () {
