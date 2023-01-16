e060f22894bb485782f41d420745a3d45285cf2f
Jin <cashleader0913@gmail.com>
2023-01-16 19:15:12 +0800

    map_end

--- JSHomeWork6/map.js
+++ JSHomeWork6/map.js
@@ -16,11 +16,11 @@ function initMap() {
     // 初始地圖
     map = L.map('map', {
         center: [25.084559885704355, 121.52489910527188],
-        zoom: 9
+        zoom: 12
     })
     // 設定圖層 openstreetmap
     let osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    let osm = new L.TileLayer(osmUrl, { minZoom: 8, maxZoom: 19 })
+    let osm = new L.TileLayer(osmUrl, { minZoom: 11, maxZoom: 25 })
     map.addLayer(osm)
 }
 
@@ -48,52 +48,49 @@ function renderMarker() {
     if (markers) markers.clearLayers();
     Object.keys(carAreaData).forEach(key => {
         let data = carAreaData[key]
-        let marker = L.marker([data[0].lat], data[0].lng)
+        let marker = L.marker([data[0].lat, data[0].lng])
         marker.bindPopup(
             `
             <h4>${data[0].Town}</h4>
             `
         )
-        console.log(data[0].Town)
-        marker.addEventListener('click', function() {
+        //console.log(data)
+        marker.addEventListener('click', function () {
             countyText.innerText = data[0].Town;
             tbody.innerHTML = '';
-
-            let nameTr = document.createElement('tr');
-            let addressTr = document.createElement('tr');
-            let otherTr = document.createElement('tr');
             data.forEach(x => {
+                let tr = document.createElement('tr');
                 let nameTd = document.createElement('td');
                 let addressTd = document.createElement('td');
+                console.log(x);
                 let otherTd = document.createElement('td');
-                nameTd.innerText = `${x.name}`
-                addressTd.innerText=`${x.address}`
-                otherTd.innerText=`${x.other}`
+                nameTd.innerText = `${x.Name}`
+                addressTd.innerText = `${x.address}`
+                otherTd.innerText = `${x.other}`
 
-                nameTr.appendChild(nameTd)
-                addressTr.appendChild(addressTd);
-                otherTr.appendChild(otherTd)
+                tr.appendChild(nameTd);
+                tr.appendChild(addressTd);
+                tr.appendChild(otherTd);
+                tbody.appendChild(tr);
             })
-            tbody.appendChild(nameTr)
-            tbody.appendChild(addressTr)
-            tbody.appendChild(otherTr)
         })
-        markers.addLayer(marker)
+        markers.addLayer(marker);
     })
-    map.addLayer(markers)
+    map.addLayer(markers);
 }
 function initCountySelect() {
-    ['請選擇', ...new Set(areaData.map(x => x.Town))].forEach(city => {
+    ['請選擇', ...new Set(areaData.filter(x => x.City == '臺北市').map(x => x.District))].forEach(district => {
         let option = document.createElement('option')
-        option.innerText = Town
-        option.value = Town == '請選擇' ? '' : Town
+        option.innerText = district
+
+        option.value = district == '請選擇' ? '' : district
         countySelect.appendChild(option)
     })
 
     countySelect.onchange = function () {
         if (this.value != '') {
             // 改變地圖的焦點
-            let county = areaData.find(x => x.Town == this.value)
+            let county = areaData.find(x => x.District == this.value)
             map.setView([county.Lat, county.Lng])
         }
     }
